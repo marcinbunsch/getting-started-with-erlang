7fb6f60831d4ce6c2ba7ce174481c048bed0e90c
Marcin Bunsch <marcin@futuresimple.com>
2012-04-25 20:52:36 +0200

    Tutorial 21 - Trapping signals

---
+++ tut21.erl
@@ -0,0 +1,42 @@
+-module(tut21).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-export([start/1, ping/2, pong/0, test/0]).
+
+ping(N, Pong_Pid) ->
+    link(Pong_Pid), 
+    ping1(N, Pong_Pid).
+
+ping1(0, _) ->
+    exit(ping);
+
+ping1(N, Pong_Pid) ->
+    Pong_Pid ! {ping, self()},
+    receive
+        pong ->
+            io:format("Ping received pong~n", [])
+    end,
+    ping1(N - 1, Pong_Pid).
+
+pong() ->
+    % we trap process exit, wo when it happens, we get a signal sent to receive
+    process_flag(trap_exit, true), 
+    pong1().
+
+pong1() ->
+    receive
+        {ping, Ping_PID} ->
+            io:format("Pong received ping~n", []),
+            Ping_PID ! pong,
+            pong1();
+        {'EXIT', From, Reason} ->
+            io:format("pong exiting, got ~p~n", [{'EXIT', From, Reason}])
+    end.
+
+start(Ping_Node) ->
+    PongPID = spawn(tut21, pong, []),
+    spawn(Ping_Node, tut21, ping, [3, PongPID]).
+
+test() ->
+    start(node()).
